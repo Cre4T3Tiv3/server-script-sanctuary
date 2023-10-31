b91b41aa1803ad06bbfa4388ae5886e608d759e3
Cre4T3Tiv3 <jlmoses@outlook.com>
2023-10-31 17:48:42 -0600

    feat(scripts): update Bash, Python, PowerShell scripts and README.md

--- python-scripts/api_consumer.py
+++ python-scripts/api_consumer.py
@@ -2,12 +2,13 @@
 api_consumer.py
 
 Task:
-Fetch and print data from 'https://jsonplaceholder.typicode.com/posts'.
+Fetch and print data from a specified web-API (default: 'https://jsonplaceholder.typicode.com/posts').
 
 Extensive Details:
 - The script uses the `requests` library to send a GET request to the specified API endpoint.
-- The `json()` method of the response object is used to parse the returned JSON data.
-- The data is then printed to the console.
+- The `json()` method of the response object is used to decode the returned JSON data into a Python data structure.
+- The data is then printed to the console, with the first 5 items being pretty-printed for readability.
+
 """
 
 import requests
@@ -18,36 +19,32 @@ API_URL = "https://jsonplaceholder.typicode.com/posts"
 
 def fetch_api_data(api_url=API_URL):
     """
-    Fetch data from an API endpoint and return it as a JSON object.
-    This function takes a URL as an argument. It sends a GET request to this URL, which is expected to be an API endpoint.
-    The function then waits for a response from the server. Once the response is received, the function parses the response
-    body as JSON and returns the resulting data.
+    Fetch data from an API endpoint and return it as a Python data structure.
+
+    This function sends a GET request to the provided URL, which is expected to be an API endpoint.
+    The function then waits for a response from the server. Once the response is received, the function decodes the response
+    body from JSON and returns the resulting data.
 
     Args:
     - api_url (str): The URL of the API endpoint. This should be a string containing a valid URL.
 
     Returns:
-    - dict: Parsed JSON data from the API. This is a dictionary that contains the data returned by the API. The structure
-            of this data will depend on the specific API endpoint that was called.
+    - dict or list: Decoded data from the API. The type and structure of this data will depend on the specific API endpoint that was called.
     """
 
     try:
         # Send a GET request to the API endpoint.
-        # The requests.get function takes a URL and sends a GET request to this URL. It then returns a Response object.
-        # This object contains the server's response to our request.
         response = requests.get(api_url)
 
         # Check if the request was successful.
         response.raise_for_status()
 
     except requests.exceptions.HTTPError as http_err:
-        print(f"The following HTTP error occurred: {http_err}")
+        print(f"HTTP error occurred: {http_err}")
     except Exception as err:
-        print(f"The following error occurred: {err}")
+        print(f"Error occurred: {err}")
     else:
-        # Parse the response body as JSON and return the resulting data.
-        # The json method of the Response object takes the response body and parses it as JSON. It then returns the resulting data.
-        # If the response body does not contain valid JSON, this method will raise an error.
+        # Decode the response body from JSON and return the resulting data.
         return response.json()
 
 
@@ -56,15 +53,15 @@ def test_fetch_api_data(api_url=API_URL):
     Test the fetch_api_data function.
 
     This function tests the fetch_api_data function with a known API endpoint and checks if the returned data is as expected.
-    The API endpoint used for testing is 'https://jsonplaceholder.typicode.com/posts'. This is a public API provided by JSONPlaceholder.
-    It's a simple fake REST API that's useful for testing and prototyping. The '/posts' endpoint returns a list of 100 fake blog posts in JSON format.
-    Each post is a dictionary with the following keys: 'userId', 'id', 'title', and 'body'. 'userId' and 'id' are integers, while 'title' and 'body' are strings.
+    The API endpoint used for testing is 'https://jsonplaceholder.typicode.com/posts', a public API provided by JSONPlaceholder.
+    The '/posts' endpoint returns a list of 100 fake blog posts in JSON format.
+
     """
 
     # Call the fetch_api_data function with the known API endpoint.
     data = fetch_api_data(api_url)
 
-    # Check if the returned data is as expected.
+    # Check if the returned data is a list, as expected.
     assert isinstance(
         data, list
     ), f"Expected a list of blog posts; received a data structure of: {type(data)}"
@@ -78,4 +75,4 @@ if __name__ == "__main__":
     if data is not None:
         print(json.dumps(data[:5], indent=4))  # Pretty-print the first 5 items.
     else:
-        print("Failed to fetch data from the website!")
+        print("Failed to fetch data from the API!")
