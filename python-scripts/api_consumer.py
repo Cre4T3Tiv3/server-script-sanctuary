3663542d89571f3517da5cd94bc46a05c78b9f80
Cre4T3Tiv3 <jlmoses@outlook.com>
2023-11-14 21:49:22 -0700

    feature(api-consumer): Enhanced Functionality with CLI, Logging, and Error Handling.

--- python-scripts/api_consumer.py
+++ python-scripts/api_consumer.py
@@ -1,79 +1,80 @@
-
-"""
-api_consumer.py
-
-Task:
-Fetch and print data from a specified web-API (default: 'https://jsonplaceholder.typicode.com/posts').
-
-Extensive Details:
-- The script uses the `requests` library to send a GET request to the specified API endpoint.
-- The `json()` method of the response object is used to decode the returned JSON data into a Python data structure.
-- The data is then printed to the console, with the first 5 items being pretty-printed for readability.
-
-"""
-
+import argparse
 import requests
 import json
+import logging
+import os
+import requests_cache
 
-API_URL = "https://jsonplaceholder.typicode.com/posts"
+# Initialize caching.
+requests_cache.install_cache("api_cache", expire_after=1800)
+
+# Configure logging.
+logging.basicConfig(
+    filename="api_consumer.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+# Configure the command-line argument parser.
+parser = argparse.ArgumentParser(description="Fetch data from an API endpoint.")
+parser.add_argument(
+    "--url",
+    type=str,
+    default="https://jsonplaceholder.typicode.com/posts",
+    help="API endpoint URL",
+)
+parser.add_argument(
+    "--apikey",
+    type=str,
+    default=os.getenv("API_KEY"),
+    help="API Key for authentication",
+)
+parser.add_argument(
+    "--timeout", type=int, default=10, help="Timeout for API request in seconds"
+)
+args = parser.parse_args()
 
 
-def fetch_api_data(api_url=API_URL):
+def fetch_api_data(api_url, api_key=None, timeout=10):
     """
     Fetch data from an API endpoint and return it as a Python data structure.
-
-    This function sends a GET request to the provided URL, which is expected to be an API endpoint.
-    The function then waits for a response from the server. Once the response is received, the function decodes the response
-    body from JSON and returns the resulting data.
-
-    Args:
-    - api_url (str): The URL of the API endpoint. This should be a string containing a valid URL.
-
-    Returns:
-    - dict or list: Decoded data from the API. The type and structure of this data will depend on the specific API endpoint that was called.
     """
-
+    headers = {"Authorization": f"Bearer {api_key}"} if api_key else {}
     try:
-        # Send a GET request to the API endpoint.
-        response = requests.get(api_url)
-
-        # Check if the request was successful.
+        logging.info(f"Sending GET request to API URL: {api_url}")
+        response = requests.get(api_url, headers=headers, timeout=timeout)
         response.raise_for_status()
-
     except requests.exceptions.HTTPError as http_err:
-        print(f"HTTP error occurred: {http_err}")
+        logging.error(
+            f"HTTP error occurred: {http_err} - Status Code: {http_err.response.status_code}"
+        )
+        return None
     except Exception as err:
-        print(f"Error occurred: {err}")
+        logging.error(f"Error occurred: {err}")
+        return None
     else:
-        # Decode the response body from JSON and return the resulting data.
+        logging.info("API data fetched successfully.")
         return response.json()
 
 
-def test_fetch_api_data(api_url=API_URL):
+def display_data(data):
     """
-    Test the fetch_api_data function.
-
-    This function tests the fetch_api_data function with a known API endpoint and checks if the returned data is as expected.
-    The API endpoint used for testing is 'https://jsonplaceholder.typicode.com/posts', a public API provided by JSONPlaceholder.
-    The '/posts' endpoint returns a list of 100 fake blog posts in JSON format.
-
+    Display data in JSON format, pretty-printing the first 5 items.
     """
+    if data:
+        print(json.dumps(data[:5], indent=4))
+    else:
+        logging.error("No data to display.")
 
-    # Call the fetch_api_data function with the known API endpoint.
-    data = fetch_api_data(api_url)
 
-    # Check if the returned data is a list, as expected.
-    assert isinstance(
-        data, list
-    ), f"Expected a list of blog posts; received a data structure of: {type(data)}"
+def main():
+    api_url = args.url
+    api_key = args.apikey
+    timeout = args.timeout
 
-    print("Test passed!")
+    data = fetch_api_data(api_url, api_key, timeout)
+    display_data(data)
 
 
 if __name__ == "__main__":
-    test_fetch_api_data()
-    data = fetch_api_data()
-    if data is not None:
-        print(json.dumps(data[:5], indent=4))  # Pretty-print the first 5 items.
-    else:
-        print("Failed to fetch data from the API!")
+    main()
